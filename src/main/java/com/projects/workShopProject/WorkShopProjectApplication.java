f0dcd8564380a16ca0a434f82cd4b9c82507f063
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-28 16:29:11 -0300

    Project created

---
+++ src/main/java/com/projects/workShopProject/WorkShopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.projects.workShopProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/workShopProject/WorkShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.workShopProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
